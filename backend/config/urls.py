9c900f04268f092818e8ea84d693a50a2fda4dd8
github VinousTy <github ys.taka44@gmail.com>
2022-04-09 16:42:20 +0900

    Change::不要なpathを削除

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authen/', include('djoser.urls.jwt')),
-    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
