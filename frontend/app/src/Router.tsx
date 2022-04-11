9393e58dfd99bc698d0c96a6cfe5ecd35dd40729
github VinousTy <github ys.taka44@gmail.com>
2022-04-11 20:28:11 +0900

    Add:プロフィール登録画面のpathを追加

--- frontend/app/src/Router.tsx
+++ frontend/app/src/Router.tsx
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import SignIn from './templates/auth/SignIn';
 import SignUp from './templates/auth/SignUp';
+import Profile from './templates/profile/Profile';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
       <Switch>
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/profile" component={Profile} />
       </Switch>
       <Footer />
     </BrowserRouter>
