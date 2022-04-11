4e9f1246587126cf2ffc89c36597b5130cd47852
github VinousTy <github ys.taka44@gmail.com>
2022-04-11 14:16:28 +0900

    Add:カラーコードの追加

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         'thin-black': 'rgba(32, 32, 32, 0.9)',
         'slid-black': '#2e2e2e',
+        'wine': '#5a09a7',
         'secondary': '#dd4b39',
       },
     },
