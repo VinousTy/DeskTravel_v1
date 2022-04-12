59e0a7f4047203df38293f2ed3ccbdca088295ed
github VinousTy <github ys.taka44@gmail.com>
2022-04-12 19:26:11 +0900

    Add:投稿を登録するページのpathを追加

--- frontend/app/src/Router.tsx
+++ frontend/app/src/Router.tsx
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import SignIn from './templates/auth/SignIn';
 import SignUp from './templates/auth/SignUp';
+import PostRegist from './templates/post/PostRegist';
 import Profile from './templates/profile/Profile';
 
 const Router = () => {
@@ -14,6 +15,7 @@ const Router = () => {
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/profile" component={Profile} />
+        <Route exact path="/post/regist" component={PostRegist} />
       </Switch>
       <Footer />
     </BrowserRouter>
