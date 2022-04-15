1dc493f10e9e54781cd27946fa69b9d12e09389f
github VinousTy <github ys.taka44@gmail.com>
2022-04-16 08:02:57 +0900

    Add:Modal内に表示する投稿リストを作成

---
+++ frontend/app/src/components/postList/editPostList/EditPostList.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { AppDispatch } from '../../../app/store';
+import { getImage, selectImage } from '../../../features/post/postSlice';
+
+interface POST_ID {
+  postId: number;
+}
+const EditPostList: React.FC<POST_ID> = ({ postId }) => {
+  const dispatch: AppDispatch = useDispatch();
+  const image = useSelector(selectImage);
+  const history = useHistory();
+
+  const img: any = image.filter((img) => {
+    return img.postId === postId;
+  });
+
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      await dispatch(getImage());
+    };
+    fetchBootLoader();
+  }, [dispatch]);
+
+  return (
+    <img
+      src={img[0]?.img}
+      alt="投稿画像"
+      onClick={() => history.push(`/post/edit/${postId}`)}
+    />
+  );
+};
+
+export default EditPostList;
