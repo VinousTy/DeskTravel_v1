d01e1d6e594d378578753a6b34c5a8824eb35267
github VinousTy <github ys.taka44@gmail.com>
2022-04-16 15:30:36 +0900

    Add:Social認証用ルートを追加

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('authen/', include('djoser.urls.jwt')),
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
