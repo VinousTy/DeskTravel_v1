f5d0b901323298f568a91061b5c886aaddd1837b
github VinousTy <github ys.taka44@gmail.com>
2022-04-16 16:42:04 +0900

    Add:パスワード変更用ルートを追加

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('authen/', include('djoser.urls.jwt')),
-    path('auth/', include('drf_social_oauth2.urls', namespace='drf'))
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('password/reset/', include('django_rest_passwordreset.urls',
+         namespace='password_reset')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
