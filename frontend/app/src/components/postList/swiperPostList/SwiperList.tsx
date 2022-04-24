2a332c8db1f6a9e5fc7833e3add1d20c75d53848
github VinousTy <github ys.taka44@gmail.com>
2022-04-24 10:12:44 +0900

    Fix:カテゴリの表示処理を修正

--- frontend/app/src/components/postList/swiperPostList/SwiperList.tsx
+++ frontend/app/src/components/postList/swiperPostList/SwiperList.tsx
@@ -100,7 +100,7 @@ const SwiperList: React.FC<PROPS_POST_LIST> = ({
   });
 
   const userCategory = categories?.filter((category?) => {
-    return category?.id === profile?.category;
+    return category?.id === prof[0]?.category;
   });
 
   useEffect(() => {
